a2d6debb679a77f0a325165f7ec4fcaa48557836
corybrunson <jabrunso@vt.edu>
2017-03-23 12:24:21 -0400

    shorten examples, don't generate empty strata

--- inst/examples/stratum.r
+++ inst/examples/stratum.r
@@ -1,34 +1,15 @@
-# basic boxes (strata) with labels
-ggplot(as.data.frame(Titanic),
-       aes(weight = Freq, axis1 = Class, axis2 = Sex, axis3 = Age)) +
-  geom_stratum() + geom_text(stat = "stratum")
-
 # full axis width
 ggplot(as.data.frame(Titanic),
        aes(weight = Freq,
            axis1 = Class, axis2 = Sex, axis3 = Age, axis4 = Survived)) +
   geom_stratum(axis_width = 1) + geom_text(stat = "stratum")
 
-# declaration of groups (unused)
-ggplot(as.data.frame(Titanic),
-       aes(weight = Freq,
-           axis1 = Class, axis2 = Sex, axis3 = Age, group = Survived)) +
-  geom_stratum() + geom_text(stat = "stratum")
-
 # use of facets
 ggplot(as.data.frame(Titanic),
        aes(weight = Freq,
-           axis1 = Class, axis2 = Sex)) +
-  geom_alluvium(aes(fill = Age)) +
+           axis1 = Class, axis2 = Sex,
+           fill = Survived)) +
+  geom_alluvium() +
   geom_stratum() + geom_text(stat = "stratum") +
   scale_x_continuous(breaks = 1:2, labels = c("Class", "Sex")) +
-  facet_wrap(~ Survived, scales = "fixed")
-
-# use of lode controls
-ggplot(as.data.frame(Titanic),
-       aes(weight = Freq,
-           axis1 = Class, axis2 = Sex, axis3 = Age)) +
-  geom_alluvium(aes(fill = Survived),
-                lode_favor = "aes", lode_order = "leftward") +
-  geom_stratum() + geom_text(stat = "stratum") +
-  scale_x_continuous(breaks = 1:3, labels = c("Class", "Sex", "Age"))
+  facet_wrap(~ Age, scales = "free_y")
