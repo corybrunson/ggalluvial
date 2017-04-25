0302da856382fb39ebe4d2cec454e001c4acf980
corybrunson <jabrunso@vt.edu>
2017-04-25 10:06:36 -0400

    credit Raff for vaccinations subset

--- R/data.r
+++ R/data.r
@@ -1,6 +1,7 @@
 #' Influenza vaccination rates
 #'
-#' This is a sample from the RAND ALP surveys on influenza vaccination.
+#' This is a sample from the RAND ALP surveys on influenza vaccination,
+#' kindly contributed by Raffaele Vardavas.
 #'
 #' @format An alluvial data frame.
 #' @source RAND American Life Panel \url{https://alpdata.rand.org/}
