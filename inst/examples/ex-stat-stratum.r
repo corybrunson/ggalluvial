7a6aa9bd33278599f7690fb1e963e3631f245366
corybrunson <jabrunso@vt.edu>
2017-05-06 08:19:29 -0400

    add default position values (pre-empt layer() errors)

--- inst/examples/ex-stat-stratum.r
+++ inst/examples/ex-stat-stratum.r
@@ -4,14 +4,14 @@ ggplot(vaccinations,
        aes(weight = freq,
            x = survey, stratum = response, alluvium = subject,
            label = freq)) +
-  geom_stratum() +
+  stat_stratum(geom = "errorbar") +
   geom_text(stat = "stratum")
 # lode data: positioning with stratum labels
 ggplot(vaccinations,
        aes(weight = freq,
            x = survey, stratum = response, alluvium = subject,
            label = response)) +
-  geom_stratum() +
+  stat_stratum(geom = "errorbar") +
   geom_text(stat = "stratum")
 
 # alluvium data: positioning with weight labels
@@ -20,7 +20,7 @@ ggplot(as.data.frame(Titanic),
            axis1 = Class, axis2 = Sex, axis3 = Age, axis4 = Survived,
            label = Freq)) +
   geom_text(stat = "stratum") +
-  geom_errorbar(stat = "stratum") +
+  stat_stratum(geom = "errorbar") +
   scale_x_continuous(breaks = 1:4,
                      labels = c("Class", "Sex", "Age", "Survived"))
 # alluvium data: positioning with stratum labels
@@ -28,6 +28,6 @@ ggplot(as.data.frame(Titanic),
        aes(weight = Freq,
            axis1 = Class, axis2 = Sex, axis3 = Age, axis4 = Survived)) +
   geom_text(stat = "stratum", label.strata = TRUE) +
-  geom_errorbar(stat = "stratum") +
+  stat_stratum(geom = "errorbar") +
   scale_x_continuous(breaks = 1:4,
                      labels = c("Class", "Sex", "Age", "Survived"))
