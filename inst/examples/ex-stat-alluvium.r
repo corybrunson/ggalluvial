d9c5d8c5e2642a3063339621e0952a28901b8a8c
corybrunson <jabrunso@vt.edu>
2017-05-14 10:17:54 -0700

    babynames example

--- inst/examples/ex-stat-alluvium.r
+++ inst/examples/ex-stat-alluvium.r
@@ -28,3 +28,24 @@ ggplot(as.data.frame(Titanic),
             lode.ordering = lode_ord) +
   geom_stratum() + geom_text(stat = "stratum", label.strata = TRUE) +
   scale_x_continuous(breaks = 1:3, labels = c("Class", "Sex", "Age"))
+
+data(babynames, package = "babynames")
+# a discontiguous alluvium
+bn <- dplyr::filter(babynames,
+                    prop >= .01 & sex == "F" &
+                      year > 1962 & year < 1968)
+ggplot(data = bn,
+       aes(x = year, alluvium = name, weight = prop)) +
+  geom_alluvium(aes(fill = name, color = name == "Tammy"),
+                decreasing = TRUE, show.legend = FALSE) +
+  scale_color_manual(values = c("#00000000", "#000000"))
+# filling in missing zeros
+bn2 <- merge(bn,
+             expand.grid(year = unique(bn$year), name = unique(bn$name)),
+             all = TRUE)
+bn2$prop[is.na(bn2$prop)] <- 0
+ggplot(data = bn2,
+       aes(x = year, alluvium = name, weight = prop)) +
+  geom_alluvium(aes(fill = name, color = name == "Tammy"),
+                decreasing = TRUE, show.legend = FALSE) +
+  scale_color_manual(values = c("#00000000", "#000000"))
