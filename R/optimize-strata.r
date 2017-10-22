654c3a0570f347b5dbac72e431eeb136c26c3092
corybrunson <jabrunso@vt.edu>
2017-10-22 11:40:03 -0400

    have optimization functions require lodes form

--- R/optimize-strata.r
+++ R/optimize-strata.r
@@ -11,58 +11,43 @@
 #'   \item \code{"area"}: the total area of the crossings
 #'   (weights have less area when the flows intersect at greater angles)
 #' }
-#' @param data Data frame.
-#' @param ... Additional parameters used to determine method and passed
-#'   thereto. All or none of \code{key}, \code{value}, and \code{id}, or else
-#'   optionally \code{axes}.
+#' @param data Data frame in lode form (see \code{\link{to_lodes}}).
+#' @param key,value,id Numeric or character; the fields of \code{data}
+#'   corresponding to the axis (variable), stratum (value), and alluvium
+#'   (identifying) variables.
 #' @param weight Optional numeric or character; the fields of \code{data}
 #'   corresponding to alluvium or lode weights (heights when plotted). The
 #'   default value \code{NULL} assigns every alluvium equal weight.
 #' @param objective Character, the objective function to minimize; matched to
 #'   \code{"count"}, \code{"weight"}, or \code{"area"}.
-#' @param exhaustive Logical; whether to exhaust all permutations of all axes,
-#'   versus to perform heuristic optimization from random initial permutations.
-#' @param niter Positive integer; the number of iterations to perform from
-#'   randomly sampled seed permutation sets.
+#' @param method Character; whether to exhaust all permutations of all axes
+#'   (\code{"exhaustive"}) versus to perform heuristic optimization from random
+#'   initial permutations (\code{"heuristic"}).
+#' @param niter Positive integer; if \code{method == "heuristic"}, the number of
+#'   iterations to perform from randomly sampled seed permutation sets.
 #' @export
 optimize_strata <- function(
-  data, ..., weight = NULL, objective = "count", exhaustive = TRUE, niter = 10
+  data, key, value, id, weight = NULL,
+  objective = "count", method = "exhaustive", niter = 10
 ) {
   
-  if (is.numeric(weight)) weight <- names(data)[weight]
-  
-  type <- is_alluvial(data, ..., weight = weight, logical = FALSE)
-  if (type == "none") {
-    stop("Data is not in a recognized alluvial form ",
-         "(see `help(is_alluvial)` for details).")
-  }
-  
-  # ensure that data is in lode form
-  if (type == "alluvia") {
-    
-    axes <- if (missing(axes)) {
-      setdiff(names(data), weight)
-    }
-    
-    data <- to_lodes(data = data,
-                     key = "x", value = "stratum", id = "alluvium",
-                     axes = axes)
-    # positioning requires numeric 'x'
-    data$x <- as.numeric(as.factor(data$x))
+  if (!is_alluvial_lodes(data = data, key = key, value = value, id = id,
+                         weight = weight, logical = TRUE)) {
+    stop("Data must be alluvial and in 'lodes' form.")
   }
   
   # governing parameters
-  xs <- sort(unique(data$x))
+  xs <- sort(unique(data[[key]]))
   n_cats <- sapply(xs, function(x) {
-    length(unique(data[data$x == x, ]$stratum))
+    length(unique(data[data[[key]] == x, ][[value]]))
   })
   
   # convert stratum values to numeric by axis
-  #data$stratum <- as.numeric(as.factor(data$stratum))
+  #data[[value]] <- as.numeric(as.factor(data[[value]]))
   #for (i in seq_along(xs)) {
-  #  data$stratum[data$x == xs[i]] <- match(
-  #    data$stratum[data$x == xs[i]],
-  #    sort(unique(data$stratum[data$x == xs[i]]))
+  #  data[[value]][data[[key]] == xs[i]] <- match(
+  #    data[[value]][data[[key]] == xs[i]],
+  #    sort(unique(data[[value]][data[[key]] == xs[i]]))
   #  )
   #}
   
@@ -71,18 +56,19 @@ optimize_strata <- function(
     "count", "weight", "area"
   ))))
   perms <- lapply(n_cats, seq_len)
-  obj <- objective_fun(data, perms)
+  obj <- objective_fun(data, key, value, id, perms)
   
-  if (exhaustive) {
+  method <- match.arg(method, c("exhaustive", "heuristic"))
+  if (method == "exhaustive") {
     rev_perms <- lapply(lapply(n_cats, seq_len), rev)
     repeat {
-      if (identical(new_perms, rev_perms) break
-          new_perms <- increment_permutations(perms)
-          new_obj <- objective_fun(data, new_perms)
-          if (new_obj < obj) {
-            perms <- new_perms
-            obj <- new_obj
-          }
+      if (identical(new_perms, rev_perms)) break
+      new_perms <- increment_permutations(perms)
+      new_obj <- objective_fun(data, key, value, id, new_perms)
+      if (new_obj < obj) {
+        perms <- new_perms
+        obj <- new_obj
+      }
     }
   } else {
     for (i in seq_along(niter)) {
@@ -108,12 +94,12 @@ optimize_strata_alluvia <- function(data, objective_fun, init) {
   repeat {
     step <- FALSE
     perms <- init
-    obj <- objective_fun(data, perms)
+    obj <- objective_fun(data, key, value, id, perms)
     for (i in seq_along(init)) {
       for (j in seq_along(init[[i]])[-1]) {
         new_perms <- init
         new_perms[[i]][c(j - 1, j)] <- new_perms[[i]][c(j, j - 1)]
-        new_obj <- objective_fun(data, new_perms)
+        new_obj <- objective_fun(data, key, value, id, new_perms)
         if (new_obj < obj) {
           perms <- new_perms
           obj <- new_obj
@@ -127,29 +113,34 @@ optimize_strata_alluvia <- function(data, objective_fun, init) {
   list(perms = perms, obj = obj)
 }
 
-objective_fun <- function(data, perms) {
-  xs <- sort(unique(data$x))
+# RESUME HERE
+# DELETE AFTER ADAPTING TO SPECIFIC OBJECTIVE FUNCTIONS
+objective_fun <- function(data, key, value, id, perms) {
+  xs <- sort(unique(data[[key]]))
   for (i in seq_along(xs)) {
-    data$stratum[data$x == xs[i]] <- perms[[i]][data$stratum[data$x == xs[i]]]
+    this_axis <- which(data[[key]] == xs[i])
+    these_values <- sort(unique(data[[value]][this_axis]))
+    data[[value]][this_axis] <-
+      these_values[perms[[i]][as.numeric(droplevels(data[[value]][this_axis]))]]
   }
   obj <- 0
   for (i in seq_along(xs)[-1]) {
-    d_left <- data[data$x == xs[i - 1], c("alluvium", "stratum")]
-    p_left <- d_left$alluvium[order(d_left$stratum)]
-    d_right <- data[data$x == xs[i], c("alluvium", "stratum")]
-    p_right <- d_right$alluvium[order(d_right$stratum)]
+    d_left <- data[data[[key]] == xs[i - 1], c(id, value)]
+    p_left <- d_left[[id]][order(d_left[[value]])]
+    d_right <- data[data[[key]] == xs[i], c(id, value)]
+    p_right <- d_right[[id]][order(d_right[[value]])]
     
   }
 }
 
-objective_count <- function(data, perms) {
+objective_count <- function(data, key, value, id, perms) {
   
 }
 
-objective_weight <- function(data, perms) {
+objective_weight <- function(data, key, value, id, perms) {
   
 }
 
-objective_area <- function(data, perms) {
+objective_area <- function(data, key, value, id, perms) {
   
 }
