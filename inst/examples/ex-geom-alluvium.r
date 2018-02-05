232afc08d45c9d8f7cd32b5b8431556230dafcbd
corybrunson <jabrunso@vt.edu>
2018-02-05 07:29:57 -0500

    use scale_x_discrete for categorical axis set

--- inst/examples/ex-geom-alluvium.r
+++ inst/examples/ex-geom-alluvium.r
@@ -4,7 +4,7 @@ ggplot(as.data.frame(Titanic),
            axis1 = Class, axis2 = Sex, axis3 = Age,
            fill = Survived)) +
   geom_alluvium() +
-  scale_x_continuous(breaks = 1:3, labels = c("Class", "Sex", "Age"))
+  scale_x_discrete(limits = c("Class", "Sex", "Age"))
 
 gg <- ggplot(alluvial::Refugees,
              aes(weight = refugees, x = year, alluvium = country))
@@ -12,5 +12,5 @@ gg <- ggplot(alluvial::Refugees,
 gg + geom_alluvium(aes(fill = country, colour = country),
                    width = 1/4, alpha = 2/3, decreasing = FALSE)
 # time series line plot of refugees data, sorted by country
-gg + geom_alluvium(aes(fill = country),
-                   colour = "black", decreasing = NA, width = 0, knot.pos = 0)
+gg + geom_alluvium(aes(fill = country, colour = country),
+                   decreasing = NA, width = 0, knot.pos = 0)
--- inst/examples/ex-geom-flow.r
+++ inst/examples/ex-geom-flow.r
@@ -3,7 +3,7 @@ ggplot(as.data.frame(Titanic),
        aes(weight = Freq,
            axis1 = Class, axis2 = Sex, axis3 = Age)) +
   geom_flow() +
-  scale_x_continuous(breaks = 1:3, labels = c("Class", "Sex", "Age")) +
+  scale_x_discrete(limits = c("Class", "Sex", "Age")) +
   geom_stratum() + geom_text(stat = "stratum", label.strata = TRUE) +
   ggtitle("Alluvial diagram of Titanic passenger demographic data")
 
@@ -13,7 +13,7 @@ ggplot(as.data.frame(Titanic),
            axis1 = Class, axis2 = Sex)) +
   geom_flow(aes(fill = Age)) +
   geom_stratum() + geom_text(stat = "stratum", label.strata = TRUE) +
-  scale_x_continuous(breaks = 1:2, labels = c("Class", "Sex")) +
+  scale_x_discrete(limits = c("Class", "Sex")) +
   facet_wrap(~ Survived, scales = "fixed")
 
 # time series alluvia of WorldPhones data
