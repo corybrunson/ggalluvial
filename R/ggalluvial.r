d0b63769880ba350b7111c89d8d256898ca2b335
Jason Cory Brunson <cornelioid@gmail.com>
2019-09-06 13:56:11 -0400

    credit uconn health in titular documentation

--- R/ggalluvial.r
+++ R/ggalluvial.r
@@ -1,2 +1,13 @@
 #' @keywords internal
+#'
+
+#' @section Acknowledgments:
+#'
+
+#' Many users identified problems and suggested improvements via email and the
+#' GitHub issue tracker
+#'
+#' Development benefitted from the use of equipment and the support of
+#' colleagues at [UConn Health](https://health.uconn.edu/).
+#' 
 "_PACKAGE"
