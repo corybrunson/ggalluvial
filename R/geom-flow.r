b25309290eabe7c3bf1bf6bf619c400cb5b49a46
corybrunson <jabrunso@vt.edu>
2020-02-16 10:58:48 -0800

    test interpolate aesthetic flow option

--- R/geom-flow.r
+++ R/geom-flow.r
@@ -28,13 +28,14 @@ geom_flow <- function(mapping = NULL,
                       width = 1/3,
                       knot.pos = 1/4, knot.fix = FALSE,
                       curve = "xspline", reach = NULL, segments = NULL,
+                      space = "rgb", interpolate = "linear",
                       aes.flow = "forward",
                       na.rm = FALSE,
                       show.legend = NA,
                       inherit.aes = TRUE,
                       ...) {
   
-  aes.flow <- match.arg(aes.flow, c("forward", "backward"))
+  aes.flow <- match.arg(aes.flow, c("forward", "backward", "interpolate"))
   
   layer(
     geom = GeomFlow,
@@ -51,6 +52,8 @@ geom_flow <- function(mapping = NULL,
       curve = curve,
       reach = reach,
       segments = segments,
+      space = space,
+      interpolate = interpolate,
       aes.flow = aes.flow,
       na.rm = na.rm,
       ...
@@ -89,27 +92,50 @@ GeomFlow <- ggproto(
   draw_panel = function(self, data, panel_params, coord,
                         width = 1/3, aes.flow = "forward",
                         knot.pos = 1/4, knot.fix = FALSE,
-                        curve = "xspline", reach = NULL, segments = NULL) {
+                        curve = "xspline", reach = NULL, segments = NULL,
+                        space = "rgb", interpolate = "linear") {
+    save(data, panel_params, coord,
+         width, aes.flow, knot.pos, knot.fix, curve, reach, segments,
+         space, interpolate,
+         file = "draw-panel.rda")
+    load("draw-panel.rda")
     
     # exclude one-sided flows
     data <- data[complete.cases(data), ]
+    
+    # ensure compatibility among parameters
+    if (aes.flow == "interpolate" && curve == "xspline") {
+      stop("Cannot interpolate flow aesthetics along an x-spline curve; ",
+           "use an alternative curve instead.")
+    }
+    if (curve != "xspline") {
+      # default to 48 segments per curve, ensure the minimum number of segments
+      if (is.null(segments)) segments <- 48 else if (segments < 3) {
+        #warning("Must use at least 3 segments; substituting `segments = 3`.")
+        segments <- 3
+      }
+    }
     
     # adjoin data with itself by alluvia along adjacent axes
     flow_pos <- intersect(names(data), c("x", "xmin", "xmax", "width",
                                          "y", "ymin", "ymax", "knot.pos"))
     flow_aes <- intersect(names(data), c("size", "linetype",
                                          "colour", "fill", "alpha"))
-    flow_fore <- if (aes.flow != "backward") flow_aes else NULL
-    flow_back <- if (aes.flow != "forward") flow_aes else NULL
+    flow_fore <- if (aes.flow == "forward") flow_aes else NULL
+    flow_back <- if (aes.flow == "backward") flow_aes else NULL
+    flow_link <- c(flow_pos, if (aes.flow == "interpolate") flow_aes)
     data <- self_adjoin(
       data = data, key = "x", by = "alluvium",
-      link = flow_pos,
+      link = flow_link,
       keep.x = flow_fore, keep.y = flow_back,
       suffix = c(".0", ".1")
     )
     
     # aesthetics (in prescribed order)
-    aesthetics <- intersect(.color_diff_aesthetics, names(data))
+    aesthetics <- intersect(
+      if (aes.flow == "interpolate") paste0(flow_aes, ".0") else flow_aes,
+      names(data)
+    )
     # arrange data by aesthetics for consistent (reverse) z-ordering
     data <- data[do.call(order, lapply(
       data[, c("step", aesthetics)],
@@ -123,15 +149,38 @@ GeomFlow <- ggproto(
       f_path <- row_to_curve(row$xmax.0, row$xmin.1,
                              row$ymin.0, row$ymax.0, row$ymin.1, row$ymax.1,
                              row$knot.pos.0, row$knot.pos.1,
-                             curve = curve, reach = reach, segments = segments,
-                             knot.fix = knot.fix)
-      # aesthetics
-      aes <- as.data.frame(row[flow_aes], stringsAsFactors = FALSE)
-      # join aesthetics to path
-      f_data <- cbind(f_path, aes[rep(1, nrow(f_path)), ])
+                             knot.fix = knot.fix,
+                             curve = curve, reach = reach, segments = segments)
+      
+      if (aes.flow == "interpolate") {
+        # -+- interpolate values in suitable ways -+-
+        aes_fore <- data.frame(
+          fill = seq_col(row$fill.0, row$fill.1, length.out = segments+1,
+                         space = space, interpolate = interpolate),
+          size = seq(row$size.0, row$size.1, length.out = segments+1),
+          linetype = rep(
+            c(row$linetype.0, row$linetype.1),
+            c(ceiling((segments+1) / 2), floor((segments+1) / 2))
+          ),
+          colour = seq_col(row$colour.0, row$colour.1, length.out = segments+1,
+                           space = space, interpolate = interpolate),
+          alpha = seq(row$alpha.0, row$alpha.1, length.out = segments+1),
+          stringsAsFactors = FALSE
+        )
+        aes <- rbind(aes_fore, aes_fore[seq(nrow(aes_fore), 1), , drop = FALSE])
+        # join aesthetics to path
+        f_data <- cbind(f_path, aes)
+      } else {
+        # uniform aesthetics
+        aes <- as.data.frame(row[flow_aes], stringsAsFactors = FALSE)
+        # join aesthetics to path
+        f_data <- cbind(f_path, aes[rep(1, nrow(f_path)), ])
+      }
       
       # transform (after calculating spline paths)
       f_coords <- coord$transform(f_data, panel_params)
+      
+      # -+- construct separate polygons and segments for interpolation -+-
       
       # single spline grob
       grid::xsplineGrob(
@@ -163,11 +212,6 @@ row_to_curve <- function(
     row_to_xspline(x0, x1, ymin0, ymax0, ymin1, ymax1,
                    kp0, kp1, knot.fix)
   } else {
-    # default to 48 segments per curve, ensure the minimum number of segments
-    if (is.null(segments)) segments <- 48 else if (segments < 3) {
-      #warning("Must use at least 3 segments; substituting `segments = 3`.")
-      segments <- 3
-    }
     # unit curve path
     row_to_unit_curve(x0, x1, ymin0, ymax0, ymin1, ymax1,
                       curve, reach, segments)
@@ -202,4 +246,11 @@ row_to_unit_curve <- function(
           ymax1 + (ymax0 - ymax1) * f_fore),
     shape = 0
   )
+}
+
+seq_col <- function(from, to, length.out, ...) {
+  if (from == 0 && to == 0) return (rep(0, length.out))
+  crf <- colorRamp(c(from, to), ...)
+  arr <- crf(seq(0, 1, length.out = length.out))
+  apply(arr, 1, function(x) rgb(x[1], x[2], x[3], maxColorValue = 256))
 }
