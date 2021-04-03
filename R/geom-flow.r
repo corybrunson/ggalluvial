4cc3c6d16b2047d4faf6a0a8ecf9ad71891d4d80
corybrunson <cornelioid@gmail.com>
2021-04-03 10:17:22 -0400

    refactor(geom): default colour to 'transparent' rather than 0 - fixes #78

--- R/geom-flow.r
+++ R/geom-flow.r
@@ -69,7 +69,7 @@ GeomFlow <- ggproto(
   required_aes = c("x", "y", "ymin", "ymax"),
 
   default_aes = aes(size = .5, linetype = 1,
-                    colour = 0, fill = "gray", alpha = .5),
+                    colour = "transparent", fill = "gray", alpha = .5),
 
   setup_data = function(data, params) {
 
