a4199462366267edabf272e60a4f423eec94ed40
Jason Cory Brunson <cornelioid@gmail.com>
2023-07-04 16:50:58 -0400

    mention alpha default + link to aesthetics vignette - fixes #112

--- man-roxygen/geom-aesthetics.r
+++ man-roxygen/geom-aesthetics.r
@@ -14,4 +14,7 @@
 #' - `group`
 #'
 #' `group` is used internally; arguments are ignored.
-#'
+#' 
+#' Alluvium, flow, and lode geoms default to `alpha = 0.5`. Learn more about
+#' setting these aesthetics in `vignette("ggplot2-specs", package = "ggplot2")`.
+#' 
